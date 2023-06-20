b94ce5b9926375562de9b16ee34bc904072b87e8
sstfnk3107 <s.stefanyk75@gmail.com>
2023-06-20 11:21:11 +0300

    start to do task11

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import Notiflix from 'notiflix';
+
+
+const searchForm = document.querySelector('.search-form');
+const btn = 
+  
+searchForm.addEventListener('submit', )
+
+
+
+
+
+
+
+
+{/* <form class="search-form" id="search-form">
+  <input
+    type="text"
+    name="searchQuery"
+    autocomplete="off"
+    placeholder="Search images..."
+  />
+  <button type="submit">Search</button>
+</form> */}
