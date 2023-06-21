df58db330beefb3cc79a56cd79815229c99b9629
sstfnk3107 <s.stefanyk75@gmail.com>
2023-06-21 23:43:53 +0300

    11 task done

---
+++ src/api.js
@@ -0,0 +1,27 @@
+const BAS_URL = 'https://pixabay.com/api';
+const End_POINT = '/?';
+const API_KEY = '37549280-c708feca670f76edfee67d8a3';
+
+
+
+export const API = {
+  page: 1,
+  query: '',
+
+  async getImages() {
+    const Params = new URLSearchParams({
+      key: API_KEY,
+      image_type: 'photo',
+      q: this.query,
+      safesearch: 'true',
+      orientation: 'horizontal',
+      page: this.page,
+      per_page: 40,
+    });
+    const url = BAS_URL + End_POINT + Params;
+
+    this.page += 1;
+    const res = await fetch(url)
+    return res.json();
+  },
+};
--- src/index.js
+++ src/index.js
@@ -2,22 +2,85 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import { API } from './api.js';
+
+let lightbox = new SimpleLightbox('.gallery a') 
+
+
 
 const refs = {
   form: document.querySelector('.search-form'),
-  btnSubmit: document.querySelector('.submit'), 
-  
+  btnLoadmore: document.querySelector('.load-more'), 
+  gallery: document.querySelector('.gallery'),
   
 }
 
-refs.form.addEventListener('submit', handlerSubmit);
-refs.btnSubmit.addEventListener('click', handlerClick);
+refs.form.addEventListener('submit', onFormSubmit);
+refs.btnLoadmore.addEventListener('click', onLoadmore);
+
+async function onLoadmore() {
+  if (API.page > maxPage) {
+    refs.btnLoadmore.classList.add('hide');
+    
+  Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+  }
+  else {
+    const result = await API.getImages()
+    if (result.totalHits === 0)
+      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    else
+    renderImages(result.hits)
+  }
+}
+
+let maxPage = 0;
+
+async function onFormSubmit(event) {
+  event.preventDefault();
+  const query = refs.form.elements.searchQuery.value;
+  refs.gallery.innerHTML = '';
+  API.query = query;
+  API.page = 1;
+  refs.btnLoadmore.classList.add('hide');
+  const result = await API.getImages()
+    maxPage = result.totalHits / 40
+  if (result.totalHits === 0)
+    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+  else {
+    renderImages(result.hits)
+    refs.btnLoadmore.classList.remove('hide');
+  }
+}
 
 
+function renderImages(images) {
+  const markup = creatMarkup(images)
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
 
-
-
-
+function creatMarkup(images) {
+  return images.map((el) => {
+    return `
+    <div class="photo-card">
+  <a href="${el.largeImageURL}" ><img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${el.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${el.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${el.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${el.downloads}
+    </p>
+  </div>
+</div>`
+  }).join('');
+}
 
 
 {/* <form class="search-form" id="search-form">
