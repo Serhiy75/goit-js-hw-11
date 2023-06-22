db7c97107f904df0c0c714e7c631e91f2ab902fd
sstfnk3107 <s.stefanyk75@gmail.com>
2023-06-22 15:18:29 +0300

    add axios

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { API } from './api.js';
+import axios from "axios";
 
 let lightbox = new SimpleLightbox('.gallery a') 
 
